57989fabfa53c52d7e283d6b7d70e776270b99c1
jmedran <jmedran@gmail.com>
2018-02-15 10:56:04 -0700

    picking a winner

--- src/App.js
+++ src/App.js
@@ -37,6 +37,17 @@ class App extends Component {
     this.setState({ message: 'You have been entered' })
   };
 
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({message: 'Waiting on transaction success'});
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({message: 'A winner has been picked'});
+  };
+
   render() {
     return (
      <div>
@@ -56,6 +67,9 @@ class App extends Component {
        </form>
        <hr />
        <h1>{this.state.message}</h1>
+       <hr />
+       <h4>Ready to pick a winner?</h4>
+       <button onClick={this.onClick}>Pick a winner!</button>
      </div>
     );
   }
